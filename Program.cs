36e86a491db456adb470de66cb75988d57ec34d5
Joshua Fronsee <jbfronsee@gmail.com>
2025-12-16 12:42:03 -0700

    Generates Histogram of test image

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ImageMagick;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Processing Image...");
+
+        var imageFromFile = new MagickImage("test.png");
+
+        var hist = imageFromFile.Histogram();
+
+        foreach (var color in hist)
+        {
+            Console.WriteLine($"Color: {color.Key} Occurences: {color.Value}");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Building
+
+1. Install Magick.NET package
+    - `dotnet add package Magick.NET-Q8-AnyCPU`
+2. Build Project
+    - `dotnet publish -c Release -r <RID>`
+    - Linux RID: `linux-x64`
+    - Windows RID: `win-x64`
+
+# Run
+`./bin/Release/net8.0/<RID>/publish/PaletteGen`
+
+
